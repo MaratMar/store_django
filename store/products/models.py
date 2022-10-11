21ba8a2f17d6c6a4a5ce1a05fa223b7f221704eb
Mmrat <m.a.marfin.w@gmail.com>
2022-10-11 18:25:25 +0700

    add basket

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from users.models import User
 
 # Create your models here.
 class ProductCategory(models.Model):
@@ -24,3 +25,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} | {self.category.name}"
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Корзина для {self.user.username} | Продукт {self.product.name}"
--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from products.models import Product, ProductCategory
+from django.shortcuts import render, HttpResponseRedirect
+from products.models import Product, ProductCategory, Basket
 
 
 # Create your views here.
@@ -17,3 +17,25 @@ def products(request):
         'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context=context)
+
+
+def basket_add(request, product_id):
+    current_page = request.META.get('HTTP_REFERER')
+
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(current_page)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(current_page)
+
+
+def basket_delete(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
